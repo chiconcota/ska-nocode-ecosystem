d92d0f859356832f639d8ac47b371e5bdee83c9c
chiconcota <lytatthanh@gmail.com>
2026-05-23 06:40:28 +0700

    Clean up plugin directories: remove debug/test scripts and diff text files

--- wp-content/plugins/ska-no-code-design/flush-cache.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../../../wp-load.php';
-if (class_exists('\Ska\Design\Api\Organisms_API')) {
-    \Ska\Design\Api\Organisms_API::get_instance()->export_physical_cache();
-    echo "Cache flushed!";
-} else {
-    echo "Class not found!";
-}
--- wp-content/plugins/ska-no-code-design/test-db-2.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../../../wp-load.php';
-global $wpdb;
-$organism = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}ska_data_sys_organisms WHERE id = 85", ARRAY_A);
-echo json_encode($organism, JSON_PRETTY_PRINT);
